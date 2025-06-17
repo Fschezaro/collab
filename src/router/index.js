f1183b8b29a0a31d1b67188ba234c5855c26443e
Fernando Schezaro <130468900+Fschezaro@users.noreply.github.com>
2025-06-17 13:17:08 -0300

    Deploy in githubpages

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import MerchView from '@/views/MerchView.vue'
 import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
